7ef0c6ab38014b8de950e990ab9a1f3be97491b9
nicolasm <nicolas.mol76@icloud.com>
2023-10-24 13:16:04 +0000

    _

---
+++ core/Config.ts
@@ -0,0 +1,15 @@
+const Config = {
+  anonymeAdsGroupName: "𝘿𝙅𝙊 • Tests",
+  keywords: {
+    iamKeyWord: "==",
+  },
+  owner: "22559718434@c.us",
+  useChatGPT: true,
+  chatGPTmaxHistory: 10,
+  chatGPTmaxTokens: 150,
+  chatGPTtemperature: 0.5,
+  chatGPTEndPoint: "https://api.openai.com/v1/chat/completions",
+  delayBetweenRepliesInSec: 3,
+};
+
+export default Config;
